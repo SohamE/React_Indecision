75ad15fed2b654cf523f3584d087c88a3ae0517e
SohamE <soham.584sengupta@gmail.com>
2022-11-14 22:04:21 +0530

    Basic Component 2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,42 @@
 class Header extends React.Component {
   render() {
-    return <p>This is from Header</p>;
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return <p>Options component here</p>;
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return <p>AddOption component here</p>;
   }
 }
 
 const jsx = (
   <div>
-    <h1>Title</h1>
     <Header />
+    <Action />
+    <Options />
+    <AddOption />
   </div>
 );
 
